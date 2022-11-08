8a12766aa596987791c46ef615434d66db8cdebd
helmics251 <117761635+helmics251@users.noreply.github.com>
2022-11-09 01:07:37 +0800

    Update assignment.c

--- assignment.c
+++ assignment.c
@@ -15,24 +15,24 @@ int main()
 	int pipefd1[2]; 
 	int pipefd2[2];
 	int pipefd3[2];
-    char stuID[100];
-    char roomNUM[100];
-    char name[100];
-    char s[200];
-    signal(SIGINT, sighandler);
+    	char stuID[100];
+    	char roomNUM[100];
+    	char name[100];
+    	char s[200];
+    	signal(SIGINT, sighandler);
 
 	if (pipe(pipefd1) == -1) 
-  {
+  	{
 		fprintf(stderr, "Pipe 1 Failed");
 		return 1;
 	}
 	if (pipe(pipefd2) == -1) 
-  {
+ 	{
 		fprintf(stderr, "Pipe 2 Failed");
 		return 1;
 	}
 	if (pipe(pipefd3) == -1) 
-  {
+  	{
 		fprintf(stderr, "Pipe 3 Failed");
 		return 1;
 	}
@@ -41,29 +41,29 @@ int main()
 	pid_t pid2 = fork();
 
 	if (pid1 > 0 && pid2 > 0) 
-  {
+  	{
 		close(pipefd1[0]);
 		close(pipefd2[0]);
 		close(pipefd3[0]);
 		
 		printf("Enter Your Student ID: \n");
-        scanf("%s", stuID); //get input from keyboard
-        printf("\n");
-        printf("Enter Room Number: \n");
-        scanf("%s", roomNUM);
-        printf("\n");
-        printf("Enter Name: \n");
-        scanf("%s", name); //get input from keyboard
-        printf("\n");
+        	scanf("%s", stuID); //get input from keyboard
+        	printf("\n");
+        	printf("Enter Room Number: \n");
+        	scanf("%s", roomNUM);
+        	printf("\n");
+        	printf("Enter Name: \n");
+        	scanf("%s", name); //get input from keyboard
+        	printf("\n");
         
-        write(pipefd1[1], stuID, 100);
-        write(pipefd2[1], roomNUM, 100);
-        write(pipefd3[1], name, 100);
+        	write(pipefd1[1], stuID, 100);
+        	write(pipefd2[1], roomNUM, 100);
+        	write(pipefd3[1], name, 100);
         
-        close(pipefd1[1]);
-        close(pipefd2[1]);
-        close(pipefd3[1]);
-	    wait(NULL);
+        	close(pipefd1[1]);
+        	close(pipefd2[1]);
+        	close(pipefd3[1]);
+		wait(NULL);
 	}
 	else if (pid1 == 0 && pid2 > 0)
 	{
@@ -80,7 +80,8 @@ int main()
 		close(pipefd2[0]);
 		
 	}
-	else {
+	else 
+	{
 		close(pipefd3[1]);
 		read(pipefd3[0], name, 100);
 		printf("Your Name: %s \n", name);
